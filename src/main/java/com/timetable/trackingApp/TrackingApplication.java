e5b1e9932409b3baeeee292944a971c5c2497bd8
DenysChaykovskyy <dickchest@gmail.com>
2024-05-22 18:17:57 +0200

    removed  2ServiceAccountKey.json

--- src/main/java/com/timetable/trackingApp/TrackingApplication.java
+++ src/main/java/com/timetable/trackingApp/TrackingApplication.java
@@ -1,13 +1,35 @@
 package com.timetable.trackingApp;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.google.firebase.FirebaseOptions;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Objects;
 
 @SpringBootApplication
 public class TrackingApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		ClassLoader classLoader = TrackingApplication.class.getClassLoader();
+
+		File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+
+		FileInputStream serviceAccount =
+				new FileInputStream(file.getPath());
+
+		FirebaseOptions options = new FirebaseOptions.Builder()
+				.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.build();
+
+		FirebaseApp.initializeApp(options);
+
+
 		SpringApplication.run(TrackingApplication.class, args);
 	}
-
 }
