f32a10273975ce0fdff5a9d776128bba30e64bd2
DenysChaykovskyy <dickchest@gmail.com>
2024-05-23 02:08:12 +0200

    Update FirebaseConfig

--- src/main/java/com/timetable/trackingApp/configuration/FirebaseConfig.java
+++ src/main/java/com/timetable/trackingApp/configuration/FirebaseConfig.java
@@ -3,28 +3,33 @@ package com.timetable.trackingApp.configuration;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 import javax.annotation.PostConstruct;
-import java.io.FileInputStream;
+import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
+
+    @Value("classpath:serviceAccountKey.json")
+    Resource resourceFile;
+
+    @Bean
+    public FirebaseAuth firebaseAuth() {
+        return FirebaseAuth.getInstance();
+    }
+
+
     @PostConstruct
-    public void initialize() {
-
-        try {
-            FileInputStream serviceAccount = new FileInputStream("src/main/resources/serviceAccountKey.json");
-
-            FirebaseOptions options = new FirebaseOptions.Builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
-            if (FirebaseApp.getApps().isEmpty()) {
-                FirebaseApp.initializeApp(options);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void initializeFirebaseApp() throws IOException {
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(resourceFile.getInputStream()))
+                .setServiceAccountId("firebase-adminsdk-qtm0u@time-tracking-app-790ea.iam.gserviceaccount.com")
+                .build();
+        FirebaseApp.initializeApp(options);
     }
 }
