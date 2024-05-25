0f464f89d149110cfec13ee73187c94fc0a7ddb7
DenysChaykovskyy <dickchest@gmail.com>
2024-05-25 16:46:51 +0200

    ReviewService

--- src/main/java/com/timetable/trackingApp/services/ReviewService.java
+++ src/main/java/com/timetable/trackingApp/services/ReviewService.java
@@ -2,12 +2,8 @@ package com.timetable.trackingApp.services;
 
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.UserRecord;
 import com.google.firebase.cloud.FirestoreClient;
 import com.timetable.trackingApp.domain.Reviews;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
@@ -16,64 +12,62 @@ import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 
 @Service
-@AllArgsConstructor
 public class ReviewService {
-    FirebaseAuthService firebaseAuthService;
+    private FirebaseAuthService firebaseAuthService;
+    private final Firestore dbFirestore = FirestoreClient.getFirestore();
+    private final CollectionReference collection = dbFirestore.collection("reviews");
 
     public List<Reviews> getAll() throws ExecutionException, InterruptedException {
-        Firestore dbFirestore = FirestoreClient.getFirestore();
-        ApiFuture<QuerySnapshot> future = dbFirestore.collection("reviews").get();
+        ApiFuture<QuerySnapshot> future = collection.get();
         List<QueryDocumentSnapshot> documents = future.get().getDocuments();
 
         return documents.stream().map(x -> x.toObject(Reviews.class)).toList();
     }
 
     public String create(Reviews entity, Principal principal) {
-        Firestore dbFirestore = FirestoreClient.getFirestore();
-        DocumentReference addedDocRef = dbFirestore.collection("reviews").document();
+        DocumentReference addedDocRef = collection.document();
         entity.setId(addedDocRef.getId());
         entity.setFromUserId(firebaseAuthService.getUserUid(principal));
-        UserRecord toUserRecord;
         ApiFuture<WriteResult> writeResult = addedDocRef.set(entity);
         return addedDocRef.getId();
     }
 
-
     public Reviews get(String documentId) {
-        Firestore dbFirestore = FirestoreClient.getFirestore();
-        DocumentReference documentReference = dbFirestore.collection("reviews").document(documentId);
-        ApiFuture<DocumentSnapshot> future = documentReference.get();
-        try {
-            DocumentSnapshot document = future.get();
-            return document.toObject(Reviews.class);
-        } catch (Exception e) {
-            throw new RuntimeException("Not Found");
-        }
+        DocumentSnapshot document = checkIfExistDocument(documentId);
+        return document.toObject(Reviews.class);
     }
 
     public String update(Reviews entity) throws ExecutionException, InterruptedException {
-        Firestore dbFirestore = FirestoreClient.getFirestore();
-        // находим документ в бд
+        // проверка, есть ли документ
         Reviews request = get(entity.getId());
-        // проврка каждого поля
+        // проверяем каждое поле
         Optional.ofNullable(entity.getToUserId()).ifPresent(request::setToUserId);
         Optional.ofNullable(entity.getRating()).ifPresent(request::setRating);
         Optional.ofNullable(entity.getComment()).ifPresent(request::setComment);
 
-        ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection("reviews").document(entity.getId()).set(request);
+        ApiFuture<WriteResult> collectionsApiFuture = collection.document(entity.getId()).set(entity);
         return collectionsApiFuture.get().getUpdateTime().toString();
     }
 
     public String delete(String documentId) {
-        Firestore dbFirestore = FirestoreClient.getFirestore();
-        DocumentReference documentReference = dbFirestore.collection("reviews").document(documentId);
+        // нужно проверить, есть ли документ
+        DocumentSnapshot document = checkIfExistDocument(documentId);
+        ApiFuture<WriteResult> collectionsApiFuture = collection.document(documentId).delete();
+        return "Successfully deleted " + documentId;
+    }
+
+    private DocumentSnapshot checkIfExistDocument(String documentId) {
+        DocumentReference documentReference = collection.document(documentId);
         ApiFuture<DocumentSnapshot> future = documentReference.get();
         try {
             DocumentSnapshot document = future.get();
-            ApiFuture<WriteResult> collectionsApiFuture = documentReference.delete();
-            return "Successfully deleted";
-        } catch (Exception e) {
-            throw new RuntimeException("Not Found");
+            if (document.exists()) {
+                return document;
+            } else {
+                throw new RuntimeException("Entity Not Found");
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
         }
     }
 }
