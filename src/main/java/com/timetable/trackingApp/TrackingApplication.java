f4095e4fbdd756ec0b05d31fe9beebabbe6da1ab
DenysChaykovskyy <dickchest@gmail.com>
2024-05-30 16:07:31 +0200

    updated Tests

--- src/main/java/com/timetable/trackingApp/TrackingApplication.java
+++ src/main/java/com/timetable/trackingApp/TrackingApplication.java
@@ -1,21 +1,14 @@
 package com.timetable.trackingApp;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.google.firebase.FirebaseOptions;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Objects;
 
 @SpringBootApplication
 public class TrackingApplication {
 
-	public static void main(String[] args) throws IOException {
-		SpringApplication.run(TrackingApplication.class, args);
-	}
+    public static void main(String[] args) throws IOException {
+        SpringApplication.run(TrackingApplication.class, args);
+    }
 }
